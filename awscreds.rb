545abb431094d79cd8d0a62c1489f085b7108eea
jacobfg <10949267+jacobfg@users.noreply.github.com>
2021-11-11 20:06:51 +1100

    Brew formula update for awscreds version 0.4.7

--- awscreds.rb
+++ awscreds.rb
@@ -6,14 +6,14 @@ require_relative "./lib/private_strategy.rb"
 class Awscreds < Formula
   desc "AWS Tools for managing credentials"
   homepage "https://github.com/jacobfg/awscreds"
-  version "0.4.6"
+  version "0.4.7"
   license "Apache-2.0"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jacobfg/awscreds/releases/download/0.4.6/awscreds_0.4.6_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "b06ce2ac3e69032246133b33307c0476fe3ba4f3a723eb201a93a8810c7482c8"
+      url "https://github.com/jacobfg/awscreds/releases/download/0.4.7/awscreds_0.4.7_darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "51384bfecb7c8d5010b2eaae2d4ad14202e012a661c4f00cc717780e4d63dec3"
 
       def install
         # ldflags = %W[
@@ -34,8 +34,8 @@ class Awscreds < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/jacobfg/awscreds/releases/download/0.4.6/awscreds_0.4.6_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "55517c9d3e8bca0aba091ff694f5ac1a92e13bbbf9b0057f6cc64ff450c83e10"
+      url "https://github.com/jacobfg/awscreds/releases/download/0.4.7/awscreds_0.4.7_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      sha256 "b500f0841906bc57eb32389635af862f0aea7d7450fb9540b2e2e38172f4c2cc"
 
       def install
         # ldflags = %W[
@@ -61,7 +61,7 @@ class Awscreds < Formula
     system "#{bin}/awscreds version"
     # test version to ensure that version number is embedded in binary
     # somehow add os/arch in version output
-    assert_match "awscreds: 0.4.6 (0477b54)", shell_output("#{bin}/awscreds version")
+    assert_match "awscreds: 0.4.7 (ba46d58)", shell_output("#{bin}/awscreds version")
     # assert_match "built by #{tap.user}", shell_output("#{bin}/awscreds version")
   end
 end
