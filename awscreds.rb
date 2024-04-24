492b0bb4da169ecd8dfa978f90f3ca1ad1e0ba9b
jacobfg <10949267+jacobfg@users.noreply.github.com>
2024-04-24 21:25:01 +1000

    Brew formula update for awscreds version 0.5.1

--- awscreds.rb
+++ awscreds.rb
@@ -6,34 +6,32 @@ require_relative "./lib/private_strategy.rb"
 class Awscreds < Formula
   desc "AWS Tools for managing credentials"
   homepage "https://github.com/jacobfg/awscreds"
-  version "0.5.0"
+  version "0.5.1"
   license "Apache-2.0"
   depends_on :macos
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/jacobfg/awscreds/releases/download/0.5.0/awscreds_0.5.0_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "c2a2f5c2d4db576124dd04030343428b1ea55c3e04b59f4a0d73343d73522c7f"
+  if Hardware::CPU.intel?
+    url "https://github.com/jacobfg/awscreds/releases/download/0.5.1/awscreds_0.5.1_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "0f7d20fc6889f796eefe48dcb78788e1e9c2de5d77e75912d8ad5645e98c0ad3"
 
-      def install
-        bin.install "awscreds"
-        bash_completion.install "awscreds.bash" => "awscreds"
-        zsh_completion.install "awscreds.zsh" => "_awscreds"
-        fish_completion.install "awscreds.fish"
-        # man1.install "manpages/awscreds.1.gz"
-      end
+    def install
+      bin.install "awscreds"
+      bash_completion.install "awscreds.bash" => "awscreds"
+      zsh_completion.install "awscreds.zsh" => "_awscreds"
+      fish_completion.install "awscreds.fish"
+      # man1.install "manpages/awscreds.1.gz"
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/jacobfg/awscreds/releases/download/0.5.0/awscreds_0.5.0_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-      sha256 "c884ee625cc428e60955f9f7506265a3203547586380ccae03ada29b71ffa78f"
+  end
+  if Hardware::CPU.arm?
+    url "https://github.com/jacobfg/awscreds/releases/download/0.5.1/awscreds_0.5.1_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "8e08250bbbc3c6818f428154006528d72422e91163cc729cf1c21abe42466b9c"
 
-      def install
-        bin.install "awscreds"
-        bash_completion.install "awscreds.bash" => "awscreds"
-        zsh_completion.install "awscreds.zsh" => "_awscreds"
-        fish_completion.install "awscreds.fish"
-        # man1.install "manpages/awscreds.1.gz"
-      end
+    def install
+      bin.install "awscreds"
+      bash_completion.install "awscreds.bash" => "awscreds"
+      zsh_completion.install "awscreds.zsh" => "_awscreds"
+      fish_completion.install "awscreds.fish"
+      # man1.install "manpages/awscreds.1.gz"
     end
   end
 
@@ -41,7 +39,7 @@ class Awscreds < Formula
     system "#{bin}/awscreds version"
     # test version to ensure that version number is embedded in binary
     # somehow add os/arch in version output
-    assert_match "awscreds: 0.5.0 (a243cc5)", shell_output("#{bin}/awscreds version")
+    assert_match "awscreds: 0.5.1 (215be4f)", shell_output("#{bin}/awscreds version")
     # assert_match "built by #{tap.user}", shell_output("#{bin}/awscreds version")
   end
 end
