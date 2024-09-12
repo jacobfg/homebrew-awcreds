3b8c1dc4faf614d7bd2e27d12babcd51a0e28cab
jacobfg <10949267+jacobfg@users.noreply.github.com>
2024-09-12 19:47:38 +1000

    Brew formula update for aws-sso-profile version 0.0.19

--- aws-sso-profile.rb
+++ aws-sso-profile.rb
@@ -6,13 +6,13 @@ require_relative "./lib/private_strategy.rb"
 class AwsSsoProfile < Formula
   desc "AWS tools for managing terminal profiles"
   homepage "https://github.com/jacobfg/aws-sso-profile"
-  version "0.0.18"
+  version "0.0.19"
   license "Apache-2.0"
   depends_on :macos
 
   on_intel do
-    url "https://github.com/jacobfg/aws-sso-profile/releases/download/0.0.18/aws-sso-profile_0.0.18_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "a89dd65a1f76cab07e4008d10320e22c7c40c86287ee8e0f7c1d379f953a04f6"
+    url "https://github.com/jacobfg/aws-sso-profile/releases/download/0.0.19/aws-sso-profile_0.0.19_darwin_amd64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "e2eaaf78969adfd5cacf144b5123e2289b675179a4231cc21953a5b673c375e8"
 
     def install
       # bin.install "aws-sso-profile"
@@ -31,8 +31,8 @@ class AwsSsoProfile < Formula
     end
   end
   on_arm do
-    url "https://github.com/jacobfg/aws-sso-profile/releases/download/0.0.18/aws-sso-profile_0.0.18_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "960446e81d12856993191ba2eb04fc48030f4101579a6f1c24518666dbe47975"
+    url "https://github.com/jacobfg/aws-sso-profile/releases/download/0.0.19/aws-sso-profile_0.0.19_darwin_arm64.tar.gz", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "473eaee501646f96c8a10d72c9615b1b3ffc4537aac854a81184e891f0204333"
 
     def install
       # bin.install "aws-sso-profile"
@@ -53,6 +53,6 @@ class AwsSsoProfile < Formula
 
   test do
     system "#{bin}/aws-sso-profile version"
-    assert_match "aws-sso-profile: 0.0.18 (4e43cdd)", shell_output("#{bin}/aws-sso-profile version")
+    assert_match "aws-sso-profile: 0.0.19 (2b6a840)", shell_output("#{bin}/aws-sso-profile version")
   end
 end
